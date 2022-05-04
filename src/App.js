c9a4b0aebf89faa0cbff7a9401adfdbc6f0f0a12
JozsaEmese <95234417+JozsaEmese@users.noreply.github.com>
2022-05-05 00:51:12 +0200

    takaritas, router dom telepites, main screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import MainScreen from './views/MainScreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EventHorizon</h1>
+      <Routes>
+      <Route path='/' element={<MainScreen />} />
+          <Route path='/main' element={<MainScreen />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/views/MainScreen.js
@@ -0,0 +1,10 @@
+const MainScreen = () => {
+    return <>
+
+        <div className="main">
+            <h2>MainScreen</h2>
+        </div>
+    </>
+};
+
+export default MainScreen;
