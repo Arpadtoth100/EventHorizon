762abb8cecdbb4e9f2aeccb02eaf2bab6df2ceaf
Marcus Vidrányi <91785984+Masterofgoingfaster@users.noreply.github.com>
2022-05-06 16:20:51 +0200

    Updated Main Page with Events (#16)

--- src/views/MainScreen.js
+++ src/views/MainScreen.js
@@ -1,8 +1,30 @@
+import { eventCardData } from "../services/eventCardData";
+import EventCard from "../components/EventCard";
+
 const MainScreen = () => {
+
+    const cards = eventCardData.map(item => {
+        return (
+            <EventCard
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+
     return <>
 
         <div className="main">
             <h2>MainScreen</h2>
+            <h3>Recent events</h3>
+            <section className="cards-list">
+                {cards}
+            </section>
+            <h3>Paid events</h3>
+            <section className="cards-list">
+                {cards}
+            </section>
+
         </div>
     </>
 };
