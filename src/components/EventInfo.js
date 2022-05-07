4ffbfdb8bc1ccefb753bff66c7048521d40e7d68
TamasBitera <tomi.bittera93@gmail.com>
2022-05-07 10:23:57 +0200

    eventinfo and image component

---
+++ src/components/EventInfo.js
@@ -0,0 +1,20 @@
+export default function EventInfo(props) {
+
+    return (
+        <div className="event-info">
+            
+            <h3 className="eventinfo-name">
+                <p>{props.title}</p>    
+            </h3>
+            <div className="eventinfo-date">
+                {props.date}   
+            </div>
+            <div className="eventinfo-venue">
+                {props.venue}
+            </div>
+            <div className='eventinfo-button'>
+                   <button>Join</button>
+            </div>    
+        </div>
+    )
+}
