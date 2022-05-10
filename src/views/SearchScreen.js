3cc89de56446829bcb72ef87cf3aaf9eeaa3df03
ArpadToth <arpad.toth.lb@gmail.com>
2022-05-10 12:45:26 -0500

    pagination implemented with dummy data

--- src/views/SearchScreen.js
+++ src/views/SearchScreen.js
@@ -1,14 +1,14 @@
 import Map from '../components/utilities/Map';
 import SearchBar from '../components/SearchBar';
 import FilterBar from '../components/utilities/FilterBar';
-import CardContainer from '../components/utilities/CardContainer';
+import Pagination from '../services/Pagination';
 
 function SearchScreen() {
   return (
     <div className="outlet_main">
       <SearchBar />
       <FilterBar />
-      <CardContainer title={'Results:'} />
+      <Pagination />
       <Map />
     </div>
   );
