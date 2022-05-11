5336110a4d4a3ec6ca94b4f0397e92b80fbaeeaa
ArpadToth <arpad.toth.lb@gmail.com>
2022-05-11 12:19:36 -0500

    slider added

--- src/views/MainScreen.js
+++ src/views/MainScreen.js
@@ -1,10 +1,18 @@
 import CardContainer from '../components/utilities/CardContainer';
 import { eventCardData } from '../services/eventCardData';
+import Slider from '../components/services/Slider';
+
+const imgSet = [
+  'https://www.w3schools.com/howto/img_nature_wide.jpg',
+  'https://www.w3schools.com/howto/img_snow_wide.jpg',
+  'https://www.w3schools.com/howto/img_lights_wide.jpg',
+  'https://www.w3schools.com/howto/img_mountains_wide.jpg',
+];
 
 const MainScreen = () => {
   return (
     <div className="outlet_main">
-      <div>Caroussel here</div>
+      <Slider imgSet={imgSet} />
       <CardContainer
         title={'Newest Events on the Horizon'}
         data={eventCardData}
