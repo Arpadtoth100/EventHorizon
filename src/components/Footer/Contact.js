4c7c88b739b77799694a29a4a255331092f30295
JozsaEmese <95234417+JozsaEmese@users.noreply.github.com>
2022-05-13 22:31:35 +0200

    aboutus, faq,privacy, contact

--- src/components/Footer/Contact.js
+++ src/components/Footer/Contact.js
@@ -1,8 +1,19 @@
 
 function Contact() {
-    return(
+    return (
         <div className="outlet_main">
-            Contact
+            <h1>Contact Us</h1>
+            <p>Most contact pages are designed with function in mind.</p>
+
+            <p>They slap an email address, phone, and location on a plain background and call it a day.</p>
+
+            <p>But basic contact pages don’t inspire visitors to reach out and connect.</p>
+
+            <p>Other pages make it easy to contact the company – which is awesome.</p>
+
+            <p>Except, that can also drive up customer service costs. </p>
+            <h3>So what makes the perfect Contact Us page?</h3>
+            <p> An awesome Contact Us page finds just the right balance between making it easy to reach the company and sharing resources users can use to answer their questions right away.</p>
         </div>
     )
 }
--- src/components/Header/AboutUs.js
+++ src/components/Header/AboutUs.js
@@ -1,5 +1,20 @@
 function AboutUs() {
-  return <div className="outlet_main">About us</div>;
+  return <div className="outlet_main">
+    <div className="about-us_container">
+      <div className="aboutus-hero" >
+        <div className="aboutus-hero-img-wrapper">
+          <img className="aboutus-hero-img" src="./pexels-pixabay-207896.jpg" alt='' />
+        </div>
+        <div className="aboutus-hero-text">
+          <p>There are always great events on the horizon!</p>
+          <p>Simply join!</p>
+        </div>
+      </div>
+      <div className="blabla">
+        <p>EventHorizon is the best lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ratione est ut nisi nihil repudiandae officia pariatur voluptatem! Culpa eligendi vitae magni amet fugit nihil officiis harum, aliquid voluptas laudantium?</p>
+      </div>
+    </div>
+  </div>;
 }
 
 export default AboutUs;
