14fe183f709c1469b2dc62380ce720e3e81eeede
Marcus <vidranyimarcus@gmail.com>
2022-05-14 15:46:35 +0200

    Main page event box data connected to firebase

--- src/views/MainScreen.js
+++ src/views/MainScreen.js
@@ -1,7 +1,8 @@
 import CardContainer from '../components/utilities/CardContainer';
 import { eventCardData } from '../services/eventCardData';
 import Slider from '../services/Slider';
-
+import { readEvent } from '../services/crud';
+import { useState, useEffect } from 'react';
 const imgSet = [
   'https://www.w3schools.com/howto/img_nature_wide.jpg',
   'https://www.w3schools.com/howto/img_snow_wide.jpg',
@@ -10,14 +11,21 @@ const imgSet = [
 ];
 
 const MainScreen = () => {
+
+  const [eventList, setEventList] = useState([]);
+
+  useEffect(() => {
+    readEvent().then(snapshot => setEventList(Object.entries(snapshot.val())));
+  }, [])
+
   return (
     <div className="outlet_main">
       <Slider imgSet={imgSet} />
       <CardContainer
         title={'Newest Events on the Horizon'}
-        data={eventCardData}
+        data={eventList}
       />
-      <CardContainer title={'Featured Events'} data={eventCardData} />
+      <CardContainer title={'Featured Events'} data={eventList} />
     </div>
   );
 };
