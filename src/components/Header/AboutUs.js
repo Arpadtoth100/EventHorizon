a2e697456874a562d28df0022ff268f2462bfebf
JozsaEmese <95234417+JozsaEmese@users.noreply.github.com>
2022-05-15 20:23:58 +0200

    kicsit mas csak

--- src/components/Header/AboutUs.js
+++ src/components/Header/AboutUs.js
@@ -1,4 +1,4 @@
-import ThankYouScreen from "../../views/ThankYouScreen";
+import ThankYou from "../utilities/ThankYou";
 
 function AboutUs() {
   return <div className="outlet_main">
@@ -16,10 +16,23 @@ function AboutUs() {
         <p>EventHorizon is the best lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque ratione est ut nisi nihil repudiandae officia pariatur voluptatem! Culpa eligendi vitae magni amet fugit nihil officiis harum, aliquid voluptas laudantium?</p>
       </div>
     </div>
-    <div className="thankyouContainer">
-      <br />
-      <ThankYouScreen />
-    </div>
+    <div>
+        <div className="thankyou">
+          <div className="thankyouTitle">
+            <br/>
+            <h1>Welcome to Event Horizon!</h1>
+            <br />
+            <div className="thankyouQuestion">
+            <h2>What do you want to do next?</h2>
+            <br />
+            </div>
+          </div>
+        </div>
+        <div className="thankyouContainer2">
+        < ThankYou />
+        </div>
+        
+      </div>
   </div>;
 }
 
--- src/views/ThankYouScreen.js
+++ src/views/ThankYouScreen.js
@@ -3,17 +3,22 @@ import ThankYou from "../components/utilities/ThankYou";
 function ThankYouScreen() {
   return (
     <div className="outlet_main">
-      <div className="thankyouContainer">
+      <div>
         <div className="thankyou">
-          <div className="thankyouText">
+          <div className="thankyouTitle">
             <br/>
             <h1>Welcome to Event Horizon!</h1>
             <br />
+            <div className="thankyouQuestion">
             <h2>What do you want to do next?</h2>
             <br />
+            </div>
           </div>
         </div>
+        <div className="thankyouContainer">
         < ThankYou />
+        </div>
+        
       </div>
     </div>
   );
