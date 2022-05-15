8e501135464438705d236abb7e62421a763a450c
JozsaEmese <95234417+JozsaEmese@users.noreply.github.com>
2022-05-15 12:16:16 +0200

    thankyou, aboutus frissitese

--- src/views/ThankYouScreen.js
+++ src/views/ThankYouScreen.js
@@ -1,26 +1,16 @@
-import InfoBox from '../components/utilities/InfoBox';
+import ThankYou from "../components/utilities/ThankYou";
 
 function ThankYouScreen() {
   return (
     <div className="outlet_main">
-      <div className='ThankYouContainer'>
-        <h1>Welcome to Event Horizon!</h1>
+      <h1>Welcome to Event Horizon!</h1>
         <br />
         <h2>What do you want to do next?</h2>
         <br />
-        <div className="infoboxcontainer">
-          <div className="infoboxcolumn">
-            <InfoBox img={"./createEvent.jpg"} navTo={'/create_event'} title={'Create Event'} />
-          </div>
-          <div className="infoboxcolumn">
-            <InfoBox img={"./browseEvents.jpg"} navTo={'/search'} title={'Browse Events'} />
-          </div>
-          <div className="infoboxcolumn">
-            <InfoBox img={"./profile-Gerd Altmann Pixabay.jpg"} navTo={'/profile'} title={'Go to profile'} />
-          </div>
-        </div>
+      <div className='ThankYouContainer'>
+        
       </div>
-
+      < ThankYou />
     </div>
   );
 }
