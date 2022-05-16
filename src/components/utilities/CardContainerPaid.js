bd991ba9c352026815eca72c15371eb79e84b885
Marcus <vidranyimarcus@gmail.com>
2022-05-16 23:22:09 +0200

    work on mainpage boxes

--- src/components/utilities/CardContainerPaid.js
+++ src/components/utilities/CardContainerPaid.js
@@ -1,27 +1,26 @@
 import EventCard from '../EventComponents/EventCard';
-import { readEvent } from '../../services/crud';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+
 
 function CardContainerPaid(props) {
 
   const navTo = useNavigate();
-  
+
 
   const clickHandler = (key) => (event) => {
     event.preventDefault();
     navTo(`/eventpage/${key}`)
 
-   }
+  }
 
-  const cards = props.data.slice(-5).map((item) => {
+  const cards = props.data.slice().map((item) => {
     const key = item[0];
-    return item[1].free === "false" &&
-    <div key={key} onClick={clickHandler(key)} ><EventCard key={key} {...item[1]} /></div>
-    
+    return item[1].free === false &&
+      <div key={key} onClick={clickHandler(key)} ><EventCard key={key} {...item[1]} /></div>
+
   });
-  
-  
+
+
   return (
     <div className="main">
       <h3>{props.title}</h3>
