7437493ac64729e179c5eeb139b4277fd67cdfe3
Marcus <vidranyimarcus@gmail.com>
2022-05-23 21:28:22 +0200

    JoinedEvents.js now uses Pagination

--- src/views/MyEventsScreen.js
+++ src/views/MyEventsScreen.js
@@ -8,7 +8,7 @@ import { AuthContext } from '../components/Context/AuthContext';
 export default function MyEventsScreen(props) {
   const authContext = useContext(AuthContext);
   const [myCreatedEventList, setMyCreatedEventList] = useState([]);
-
+  console.log(myCreatedEventList)
   useEffect(() => {
 
     authContext.loggedUserID && filterMyCreatedEvent(authContext.loggedUserID).then((snapshot) =>
