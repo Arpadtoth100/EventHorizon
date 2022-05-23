b158bf4ec6f598bbb9bb62165c72c192db2da3d6
JozsaEmese <95234417+JozsaEmese@users.noreply.github.com>
2022-05-23 23:04:33 +0200

    formok jav v1

--- src/views/UpdateUserScreen.js
+++ src/views/UpdateUserScreen.js
@@ -1,6 +1,9 @@
 import UpdateUser from '../components/UpdateUser';
 
 function UpdateUserScreen() {
-  return <UpdateUser />;
+  return (
+  <div id="updateuser_main" className="updateuser_main outlet_main" >
+  <UpdateUser />
+  </div>)
 }
 export default UpdateUserScreen;
